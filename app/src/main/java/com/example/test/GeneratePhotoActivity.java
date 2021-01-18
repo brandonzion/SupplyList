8db6516f084dccd9577e2f4dc9a65c7869294401
Brandon Sun <brandonzion.sun@gmail.com>
2021-01-18 14:09:58 -0500

    Second Commit

--- app/src/main/java/com/example/test/GeneratePhotoActivity.java
+++ app/src/main/java/com/example/test/GeneratePhotoActivity.java
@@ -1,17 +1,30 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneratePhotoActivity extends AppCompatActivity {
     int CAMERA_PIC_REQUEST = 1337;
@@ -38,16 +51,35 @@ public class GeneratePhotoActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
-        if(requestCode==CAMERA_PIC_REQUEST){
-            Bitmap thumbnail = (Bitmap)data.getExtras().get("data");
-            Drawable drawable = new BitmapDrawable(getResources(), thumbnail);
+        if(requestCode==CAMERA_PIC_REQUEST && resultCode == RESULT_OK){
+            //Bitmap image = (Bitmap)data.getExtras().get("data");
+            Bitmap image = BitmapFactory.decodeResource(context.getResources(),R.drawable.icon);
+            FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(image);
+            FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+            firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                @Override
+                public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                    ArrayList<String> lines = new ArrayList<String>();
+                    for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()){
+                        lines.add(block.getText());
 
+                    }
+                    startActivity(new Intent(GeneratePhotoActivity.this, GenerateListActivity.class).putStringArrayListExtra("textList", lines));
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(GeneratePhotoActivity.this,"Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.d("Error: ", e.getMessage());
+                }
+            });
         }
         else{
             Toast.makeText(GeneratePhotoActivity.this,"Picture not taken", Toast.LENGTH_LONG);
         }
         super.onActivityResult(requestCode,resultCode,data);
     }
+
 
     private void configurePickPhotoButton() {
         Button pickPhotoButton = findViewById(R.id.pickPhotoButton);
