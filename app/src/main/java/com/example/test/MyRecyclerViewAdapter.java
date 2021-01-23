c5a72557aaf8b1bd91b60e1ac5d48c8442d30ae2
ZionSun <zioncode251@gmail.com>
2021-01-23 12:11:14 -0500

    Deleted unused class

--- app/src/main/java/com/example/test/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/test/MyRecyclerViewAdapter.java
@@ -8,11 +8,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.graphics.Color;
 import android.widget.TextView;
 import java.util.ArrayList;
-import java.util.Collections;
-import com.example.test.ItemMoveCallback.ItemTouchHelperContract;
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyRecyclerViewHolder>
         implements ItemTouchHelperAdapter {
