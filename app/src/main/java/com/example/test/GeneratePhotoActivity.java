470a433ec718c31fcbaa8b456459da721a93174c
Brandon Sun <brandonzion.sun@gmail.com>
2021-02-23 17:24:36 -0500

    Successful Image Recognition

--- app/src/main/java/com/example/test/GeneratePhotoActivity.java
+++ app/src/main/java/com/example/test/GeneratePhotoActivity.java
@@ -4,13 +4,17 @@ import androidx.annotation.ArrayRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -19,17 +23,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeneratePhotoActivity extends AppCompatActivity {
+    public static final String TESS_DATA = "/tessdata";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess";
+    private TextView textView;
+    private TessBaseAPI tessBaseAPI;
+    private Uri outputFileDir;
+    private String mCurrentPhotoPath;
+
     int CAMERA_PIC_REQUEST = 1337;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,36 +68,15 @@ public class GeneratePhotoActivity extends AppCompatActivity {
         if(requestCode==CAMERA_PIC_REQUEST && resultCode == RESULT_OK){
             //Bitmap image = (Bitmap)data.getExtras().get("data");
             Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.supplylistsinglecolumn);
-            FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(image);
-            FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
-            firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-                @Override
-                public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                    ArrayList<String> lines = new ArrayList<String>();
-                    ArrayList<Rect> rectangles = new ArrayList<Rect>();
-                    String allText = new String();
-                    for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()){
-                        allText += block.getText();
-                        allText += '\n';
-                        rectangles.add(block.getBoundingBox());
-                    }
-                    Intent intent = new Intent(GeneratePhotoActivity.this,TextTest.class);
-                    intent.putExtra("textList",allText);
-                    intent.putExtra("rectangles",rectangles);
-                    GeneratePhotoActivity.this.startActivity(intent);
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(GeneratePhotoActivity.this,"Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    Log.d("Error: ", e.getMessage());
-                }
-            });
-        }
-        else{
-            Toast.makeText(GeneratePhotoActivity.this,"Picture not taken", Toast.LENGTH_LONG);
+            prepareTessData();
+            startOCR(image);
+            String allText = getText(image);
+            Intent intent = new Intent(GeneratePhotoActivity.this,TextTest.class);
+            intent.putExtra("textList",allText);
+            GeneratePhotoActivity.this.startActivity(intent);
         }
         super.onActivityResult(requestCode,resultCode,data);
+
     }
 
 
@@ -98,5 +88,63 @@ public class GeneratePhotoActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void prepareTessData(){
+        try{
+            File dir = getExternalFilesDir(TESS_DATA);
+            if(!dir.exists()){
+                if (!dir.mkdir()) {
+                    Toast.makeText(getApplicationContext(), "The folder " + dir.getPath() + "was not created", Toast.LENGTH_SHORT).show();
+                }
+            }
+            String fileList[] = getAssets().list("");
+            for(String fileName : fileList){
+                String pathToDataFile = dir + "/" + fileName;
+                if(!(new File(pathToDataFile)).exists()){
+                    InputStream in = getAssets().open(fileName);
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+                    byte [] buff = new byte[1024];
+                    int len ;
+                    while(( len = in.read(buff)) > 0){
+                        out.write(buff,0,len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private void startOCR(Bitmap image){
+        try{
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = 6;
+            String result = this.getText(image);
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private String getText(Bitmap bitmap){
+        try{
+            tessBaseAPI = new TessBaseAPI();
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+        String dataPath = getExternalFilesDir("/").getPath() + "/";
+        tessBaseAPI.init(dataPath, "eng");
+        tessBaseAPI.setImage(bitmap);
+        String retStr = "No result";
+        try{
+            retStr = tessBaseAPI.getUTF8Text();
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+        tessBaseAPI.end();
+        return retStr;
     }
 }
--- app/src/main/java/com/example/test/TextTest.java
+++ app/src/main/java/com/example/test/TextTest.java
@@ -34,22 +34,7 @@ public class TextTest extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle allExtras = intent.getExtras();
         String allText = (String) allExtras.get("textList");
-        ArrayList<Rect> rectangles = (ArrayList<Rect>) allExtras.get("rectangles");
         TextView myTextView = findViewById(R.id.myText);
         myTextView.setText(allText);
-        Bitmap originalImage = BitmapFactory.decodeResource(getResources(),R.drawable.supplylistsinglecolumn);
-        originalImage = originalImage.copy(Bitmap.Config.RGB_565, true);
-        Canvas canvas = new Canvas(originalImage);
-        Paint paint = new Paint();
-        paint.setAlpha(0xA0); // the transparency
-        paint.setColor(Color.RED); // color is red
-        paint.setStyle(Paint.Style.STROKE); // stroke or fill or ...
-        paint.setStrokeWidth(5); // the stroke width
-        for(int i =0;i<rectangles.size();i++){
-            canvas.drawRect(rectangles.get(i), paint);
-        }
-        ImageView imageView = findViewById(R.id.image);
-        imageView.setImageBitmap(originalImage);
-
     }
 }
