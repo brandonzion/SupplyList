c39e016c400727c900add3224440c1ec8347a69a
ZionSun <zioncode251@gmail.com>
2021-03-01 19:33:10 -0500

    commit before pull

--- app/src/main/java/com/example/test/Item.java
+++ app/src/main/java/com/example/test/Item.java
@@ -4,6 +4,7 @@ public class Item {
     private int mQty;
     private String mName;
     private String mDesc;
+    private boolean mShowMenu = false;
     public Item(int qty, String name, String desc) {
         mQty = qty;
         mName = name;
@@ -18,7 +19,11 @@ public class Item {
     public String getDesc() {
         return mDesc;
     }
-
+    public boolean isShowMenu(){return mShowMenu;}
+    public void setShowMenu(boolean showMenu){mShowMenu = showMenu;};
+    public void setQty(int numb){mQty = numb;};
+    public void setName(String text){mName = text;}
+    public void setDesc(String text){mDesc = text;}
 
 
     }
--- app/src/main/java/com/example/test/ItemDisplay.java
+++ app/src/main/java/com/example/test/ItemDisplay.java
@@ -1,18 +1,11 @@
 package com.example.test;
 
 public class ItemDisplay {
-    private int mQty;
-    private String mName;
-    private String mDesc;
-    private boolean mShowMenu = false;
 
-    public ItemDisplay(boolean showMenu) {
+
+    public ItemDisplay() {
 
     }
 
-    public boolean isShowMenu(){return mShowMenu;}
-    public void setShowMenu(boolean showMenu){mShowMenu = showMenu;};
-    public void setQty(int numb){mQty = numb;};
-    public void setName(String text){mName = text;}
-    public void setDesc(String text){mDesc = text;}
+
 }
